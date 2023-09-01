6a08a4b1a1c3d09a307f6cd3c98a6ff388c46325
Pascal <pascal.reinhardt@gmx.net>
2023-09-01 09:23:16 +0200

    Database-methoden hinzugefügt.

--- src/dao/User.java
+++ src/dao/User.java
@@ -1,4 +1,96 @@
 package dao;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class User {
+
+    public enum Sex {
+        male,
+        female
+    }
+
+
+    private int id;
+    private String firstname;
+    private String lastnname;
+    private String email;
+    private Date birthdate;
+    private Timestamp registrationDate;
+    private String password;              //hash-password
+    private Enum sex;
+    private boolean mailSent;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+
+    public String getLastnname() {
+        return lastnname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public Timestamp getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Enum getSex() {
+        return sex;
+    }
+
+    public boolean isMailSent() {
+        return mailSent;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastnname(String lastnname) {
+        this.lastnname = lastnname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setRegistrationDate(Timestamp registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setSex(Enum sex) {
+        this.sex = sex;
+    }
+
+    public void setMailSent(boolean mailSent) {
+        this.mailSent = mailSent;
+    }
 }
--- src/database/inMemoryDatabase.java
+++ src/database/inMemoryDatabase.java
@@ -1,4 +1,55 @@
 package database;
 
+import dao.User;
+
+import java.util.ArrayList;
+
 public class inMemoryDatabase {
+
+    private ArrayList<User> userDatabase;
+
+    public inMemoryDatabase() {
+        this.userDatabase = new ArrayList<>();
+    }
+
+
+    public boolean addUser(User user){
+        if(user != null){
+            return  this.userDatabase.add(user);
+        }
+        return false;
+    }
+
+    public boolean removeUser(int userId) {
+        for (User user : this.userDatabase) {
+            if (user.getId() == userId) {
+                return this.userDatabase.remove(user);
+            }
+        }
+        return false;
+    }
+
+    public User getUser(int userId) {
+        for (User user : this.userDatabase) {
+            if (user.getId() == userId) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User getUser(String email) {
+        if (email != null) {
+            for (User user : this.userDatabase) {
+                if (user.getEmail().equals(email)) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<User> getAllUser() {
+        return this.userDatabase;
+    }
 }
