4092c299cf85b2a2171121ef3fda0e35a01a0a65
Pascal <pascal.reinhardt@gmx.net>
2023-09-06 15:37:04 +0200

    Methode, die testet ob initialsierte DB leer ist, hinzugefügt.

--- test/InMemoryDataBaseTest.java
+++ test/InMemoryDataBaseTest.java
@@ -1,2 +1,17 @@
+import dao.User;
+import database.inMemoryDatabase;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InMemoryDataBaseTest {
+
+    @Test
+    public void testIfInitialDBIsEmpty() {
+        System.out.println("testIfInitialDBIsEmpty");
+        inMemoryDatabase db = new inMemoryDatabase();
+        int size = db.getAllUser().size();
+        Assert.assertTrue(size == 0);
+    }
+
+
 }
